14dd872622f872acc181d399309742c637486707
Billy Block <biblock@nmu.edu>
2024-10-18 21:25:13 -0400

    created basic layout for the program

--- main.py
+++ main.py
@@ -1,1 +1,27 @@
-print("Hello world")
+from selenium import webdriver
+
+# For the basics of Selenium I was following their guide
+# https://www.selenium.dev/selenium/docs/api/py/index.html
+
+browser = webdriver.Chrome()
+
+def store_to_json():
+    # This method will store the players name, salary, and country to a JSON object
+    pass
+
+def get_players_country():
+    # https://basketball.realgm.com/nba/players
+    # Get the players name and their country
+    pass
+
+def get_players_salary():
+    # https://hoopshype.com/salaries/players/
+    # Get the players name and their salaries
+    pass
+
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()

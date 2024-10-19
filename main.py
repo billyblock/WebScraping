01593ab62246751ee0f49f5879c2666f52e5ebcf
Billy Block <biblock@nmu.edu>
2024-10-18 22:34:09 -0400

    Extracted all player names

--- main.py
+++ main.py
@@ -12,16 +12,29 @@ def store_to_json():
     # This method will store the players name, salary, and country to a JSON object
     pass
 
+
+# https://basketball.realgm.com/nba/players
+# Get the players name and their country
 def get_players_country():
-    # https://basketball.realgm.com/nba/players
-    # Get the players name and their country
+    
     browser = webdriver.Chrome()
     browser.get("https://basketball.realgm.com/nba/players")
     
-    # Gets the first players name on the site
-    player_name = browser.find_element(By.XPATH, "//tr/td[@data-th='Player']/a").text
+    # Locate all players names
+    player_name_elements = browser.find_elements(By.XPATH, "//tr/td[@data-th='Player']/a")
+
+    # Extract and print all player names
+    player_names = []
+    for element in player_name_elements:
+        player_names.append(element.text)
+        print(element.text)
     
-    print(player_name)
+    '''
+    There is not element between Council IV and Craig so I am currently unsure why it is throwing error.
+    Ricky Council IV
+    [14276:15224:1018/223149.331:ERROR:device_event_log_impl.cc(201)] [22:31:49.331] USB: usb_service_win.cc:105 SetupDiGetDeviceProperty({{A45C254E-DF1C-4EFD-8020-67D146A850E0}, 6}) failed: Element not found. (0x490)
+    Torrey Craig
+    '''
     
     browser.quit() # Properly exit browser
 

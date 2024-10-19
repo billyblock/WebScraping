cd2a1afbcce1ca87de35c6141bb6e9bb587cd51d
Billy Block <biblock@nmu.edu>
2024-10-19 18:53:16 -0400

    Stashed all players inside of json file

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By # for locating HTML elements
-import time
+import json
 
 # For the basics of Selenium I was following their guide
 # https://www.selenium.dev/selenium/docs/api/py/index.html
@@ -13,16 +13,13 @@ import time
 # List for all players
 players = []
 
-def store_to_json():
-    # This method will store the players name, salary, and country to a JSON object
-    pass
 
+browser = webdriver.Chrome()
 
 # https://basketball.realgm.com/nba/players
 # Get the players name and their country
 def get_players_country():
     
-    browser = webdriver.Chrome()
     browser.get("https://basketball.realgm.com/nba/players")
     
     # Locate all players names
@@ -46,14 +43,12 @@ def get_players_country():
             "salary": None
         }
         players.append(player_dictionary)
-    browser.quit() # Properly exit browser
 
 
 
 # https://hoopshype.com/salaries/players/
 # Get the players name and their salaries
 def get_players_salary():
-    browser = webdriver.Chrome()
     browser.get("https://hoopshype.com/salaries/players/")
     
     # Gets both specified elements 
@@ -75,7 +70,12 @@ def get_players_salary():
 def main():
     get_players_country()
     get_players_salary()
-    print(players)
+    
+    
+    browser.quit() # Properly exit browser
+    
+    with open("player_data.json", "w") as file:
+        json.dump(players, file, indent=4)
 
 if __name__ == "__main__":
     main()
